60954064bfad654e3d0ba94087d4143db5418155
Emin Fedar <eminfedar@gmail.com>
2020-12-15 20:36:29 +0300

    Setup.py & .desktop added.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+data_files = [
+    ("/usr/share/applications/", ["tr.org.pardus.about.desktop"]),
+    ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-about.mo"]),
+    ("/usr/share/pardus/pardus-about/", ["icon.svg", "pardus-logo.svg"]),
+    ("/usr/share/pardus/pardus-about/src", ["src/main.py", "src/MainWindow.py"]),
+    ("/usr/share/pardus/pardus-about/ui", ["ui/MainWindow.glade"]),
+    ("/usr/bin/", ["pardus-about"])
+]
+
+setup(
+    name="About Pardus",
+    version="0.1.0",
+    packages=find_packages(),
+    scripts=["pardus-about"],
+    install_requires=["PyGObject"],
+    data_files=data_files,
+    author="Emin Fedar",
+    author_email="emin.fedar@pardus.org.tr",
+    description="Get info about your Pardus system.",
+    license="GPLv3",
+    keywords="about",
+    url="https://www.pardus.org.tr",
+)
