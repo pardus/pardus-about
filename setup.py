9ba4572568a16e7da72afbb95725bdd9f1e70714
Emin Fedar <eminfedar@gmail.com>
2020-12-17 00:37:13 +0300

    setup.py update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ data_files = [
     ("/usr/share/applications/", ["tr.org.pardus.about.desktop"]),
     ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-about.mo"]),
     ("/usr/share/pardus/pardus-about/", ["icon.svg", "pardus-logo.svg"]),
-    ("/usr/share/pardus/pardus-about/src", ["src/main.py", "src/MainWindow.py"]),
+    ("/usr/share/pardus/pardus-about/src", ["src/main.py", "src/MainWindow.py", "src/dump_system_info.py", "src/copy_to_home.py", "src/dump_logs.py"]),
     ("/usr/share/pardus/pardus-about/ui", ["ui/MainWindow.glade"]),
     ("/usr/bin/", ["pardus-about"])
 ]
