feed182baf1dce4d70ff90189cd3684ec8be5de9
Fatih Altun <fatih.altun@pardus.org.tr>
2022-10-25 10:46:50 +0300

    Update and automate translation structure

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
-from shutil import copyfile
+# -*- coding: utf-8 -*-
 
-copyfile("icon.svg", "pardus-about.svg")
+from setuptools import setup, find_packages
+import os, subprocess
+
+
+def create_mo_files():
+    podir = "po"
+    mo = []
+    for po in os.listdir(podir):
+        if po.endswith(".po"):
+            os.makedirs("{}/{}/LC_MESSAGES".format(podir, po.split(".po")[0]), exist_ok=True)
+            mo_file = "{}/{}/LC_MESSAGES/{}".format(podir, po.split(".po")[0], "pardus-about.mo")
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(podir + "/" + po, mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo.append(("/usr/share/locale/" + po.split(".po")[0] + "/LC_MESSAGES",
+                       ["po/" + po.split(".po")[0] + "/LC_MESSAGES/pardus-about.mo"]))
+    return mo
 
 data_files = [
     ("/usr/share/applications/", ["tr.org.pardus.about.desktop"]),
-    ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-about.mo"]),
-    ("/usr/share/locale/pt/LC_MESSAGES/", ["translations/pt/LC_MESSAGES/pardus-about.mo"]),
-    ("/usr/share/pardus/pardus-about/", ["icon.svg", "bluebackground.png", "bayrak.gif"]),
+    ("/usr/share/pardus/pardus-about/", ["pardus-about.svg", "bluebackground.png", "bayrak.gif"]),
     ("/usr/share/pardus/pardus-about/src", ["src/main.py", "src/MainWindow.py", "src/dump_system_info.sh", "src/get_system_info.sh", "src/copy_to_desktop.sh", "src/dump_logs.sh"]),
     ("/usr/share/pardus/pardus-about/ui", ["ui/MainWindow.glade"]),
     ("/usr/share/pardus/pardus-about/data", ["data/pci.ids"]),
     ("/usr/share/polkit-1/actions", ["tr.org.pardus.pkexec.pardus-about.policy"]),
     ("/usr/bin/", ["pardus-about"]),
     ("/usr/share/icons/hicolor/scalable/apps/", ["pardus-about.svg"])
-]
+] + create_mo_files()
 
 setup(
     name="pardus-about",
---
+++ update-translations.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+langs=("tr")
+
+if ! command -v xgettext &> /dev/null
+then
+	echo "xgettext could not be found."
+	echo "you can install the package with 'apt install gettext' command on debian."
+	exit
+fi
+
+
+echo "updating pot file"
+xgettext -o po/pardus-about.pot --files-from=po/files
+
+for lang in ${langs[@]}; do
+	if [[ -f po/$lang.po ]]; then
+		echo "updating $lang.po"
+		msgmerge -o po/$lang.po po/$lang.po po/pardus-about.pot
+	else
+		echo "creating $lang.po"
+		cp po/pardus-about.pot po/$lang.po
+	fi
+done
