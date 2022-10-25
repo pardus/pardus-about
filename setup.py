b024b7a199e7e1f2ba423460323a9a755f24ef1f
Fatih Altun <fatih.altun@pardus.org.tr>
2022-10-25 14:27:35 +0300

    Add missing file

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if os.path.exists(changelog):
 data_files = [
     ("/usr/share/applications/", ["tr.org.pardus.about.desktop"]),
     ("/usr/share/pardus/pardus-about/", ["pardus-about.svg", "bluebackground.png", "bayrak.gif"]),
-    ("/usr/share/pardus/pardus-about/src", ["src/main.py", "src/MainWindow.py", "src/dump_system_info.sh", "src/get_system_info.sh", "src/copy_to_desktop.sh", "src/dump_logs.sh"]),
+    ("/usr/share/pardus/pardus-about/src", ["src/main.py", "src/MainWindow.py", "src/GPU.py", "src/dump_system_info.sh", "src/get_system_info.sh", "src/copy_to_desktop.sh", "src/dump_logs.sh"]),
     ("/usr/share/pardus/pardus-about/ui", ["ui/MainWindow.glade"]),
     ("/usr/share/pardus/pardus-about/data", ["data/pci.ids", "data/servers.txt"]),
     ("/usr/share/polkit-1/actions", ["tr.org.pardus.pkexec.pardus-about.policy"]),
